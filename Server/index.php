232bac07038d21cf2d9ddef2127701ca4d2bf58c
straight-tamago <121408851+straight-tamago@users.noreply.github.com>
2023-07-14 09:50:11 +0900

    Update index.php

--- Server/index.php
+++ Server/index.php
@@ -1,19 +1,22 @@
 <?php
     if (!file_exists('./currentTime.txt')) {
-	file_put_contents("./currentTime.txt", '{}', LOCK_EX);
+	    file_put_contents("./currentTime.txt", '{}', LOCK_EX);
     }
 
     $post = file_get_contents('php://input');
     $dict = json_decode($post, true);
-
     $pre_data = json_decode(file_get_contents("./currentTime.txt"), true);
-    $pre_data["data"][$dict["udid"]] = $dict;
 
-    if (!isset($pre_data["host"])) {
+    if ($_GET["set"] == true) {
         $pre_data["host"] = $dict["udid"];
+        file_put_contents("./currentTime.txt", json_encode($pre_data));
+        echo "ok";
+    }else{
+        $pre_data["data"][$dict["udid"]] = $dict;
+        if (!isset($pre_data["host"])) {
+            $pre_data["host"] = $dict["udid"];
+        }
+        file_put_contents("./currentTime.txt", json_encode($pre_data));
+        echo json_encode($pre_data["data"][$pre_data["host"]]);
     }
-
-    file_put_contents("./currentTime.txt", json_encode($pre_data));
-
-    echo json_encode($pre_data["data"][$pre_data["host"]]);
 ?>
